e565f1dbe4b2019be916d7b713a1adb451c0d42c
pbrnx <pedrobarone2709@gmail.com>
2025-05-22 10:49:26 -0300

    teste ate 100mb

--- server/serverModules/backend.js
+++ server/serverModules/backend.js
@@ -94,7 +94,7 @@ app.get('/health', (_, res) => res.status(200).json({ status: 'ok' }));
 
 /* === Middlewares globais === */
 app.use(cors());
-app.use(express.json({ limit:'10mb' }));
+app.use(express.json({ limit:'100mb' }));
 app.use(express.urlencoded({ extended:true }));
 let lastRouteGroup = null;
 
@@ -247,7 +247,7 @@ app.get('/webhook', (req, res) => {
 
 
 // --- HANDSHAKE POST + LÓGICA DO SEU WEBHOOK ---
-app.post('/webhook', express.json({limit:'10mb'}), async (req, res) => {
+app.post('/webhook', express.json({limit:'100mb'}), async (req, res) => {
   res.setHeader('X-AdobeSign-ClientId', CLIENT_ID);
 
   // Parse seguro do corpo
