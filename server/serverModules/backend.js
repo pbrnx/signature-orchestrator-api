878bcf9efb053d0657eedae7dcf43a792db25866
pbrnx <pedrobarone2709@gmail.com>
2025-05-22 13:07:20 -0300

    reduçao para 10mb como maximo

--- server/serverModules/backend.js
+++ server/serverModules/backend.js
@@ -94,7 +94,7 @@ app.get('/health', (_, res) => res.status(200).json({ status: 'ok' }));
 
 /* === Middlewares globais === */
 app.use(cors());
-app.use(express.json({ limit:'100mb' }));
+app.use(express.json({ limit:'10mb' }));
 app.use(express.urlencoded({ extended:true }));
 let lastRouteGroup = null;
 
