655c50a6f23d98ce58efce171ec84a59d11b7631
Pedro Barone <pedrobarone2709@gmail.com>
2025-07-17 15:48:40 -0300

    static html route adjustment

--- server/serverModules/backend.js
+++ server/serverModules/backend.js
@@ -106,7 +106,7 @@ app.get('/health', (_, res) => res.status(200).json({ status: 'ok' }));
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../static')));
+app.use(express.static(path.join(__dirname, '../../static')));
 let lastRouteGroup = null;
 
 app.use((req, _, next) => {
